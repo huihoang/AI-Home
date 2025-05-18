f00ef6d7ca7ffac12c953fb4586eff7b2de20f75
anhtai1124 <tainguyenanh739@gmail.com>
2025-05-18 16:36:07 +0700

    update feed param

--- Backend/utils/adafruitService.js
+++ Backend/utils/adafruitService.js
@@ -12,13 +12,15 @@ const client = mqtt.connect(brokerUrl, {
 
 client.on("connect", () => {
   console.log("Kết nối thành công với Adafruit IO");
-  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/bbc_led`);
-  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_motion`);
-  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_humidity`);
-  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_camera`);
-  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor-temperature`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/button_door`);
   client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/button_fan`);
   client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/button_hang_clothe`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/button_led`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_camera`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_humidity`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_light`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor_motion`);
+  client.subscribe(`${process.env.ADAFRUIT_USERNAME}/feeds/sensor-temperature`);
 });
 
 export default { client };
