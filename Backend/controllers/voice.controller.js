eb20aa60cee7e15a1c262b04109b9840fbcef813
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2025-05-28 15:04:57 +0700

    response result voice-control

--- Backend/controllers/voice.controller.js
+++ Backend/controllers/voice.controller.js
@@ -66,6 +66,8 @@ const updatedVoiceStatus = async (req, res) => {
     return res.json({
       message: `Lệnh được hiểu từ giọng nói là: ${latestTopic} ${latestMessage}`,
       success: success,
+      device: latestTopic.split("-")[1], // led/fan/door
+      deviceStatus: latestMessage.toUpperCase() // ON/OFF
     });
 
   } catch (error) {
